c2b0776988ea16ea6734b0c1413bf58118e85d40
Rahul Kumar <rkumar@sequrotechnologies.com>
2022-06-18 16:52:10 +0530

    fd

---
+++ src/Pages/index.jsx
@@ -0,0 +1,16 @@
+export { default as Ecommerce } from "./Ecommerce";
+export { default as Kanban } from "./Kanban";
+export { default as Orders } from "./Orders";
+export { default as Employees } from "./Employees";
+export { default as Editor } from "./Editor";
+export { default as Customers } from "./Customers";
+export { default as ColorPicker } from "./ColorPicker";
+export { default as Calendar } from "./Calendar";
+export { default as Area } from "./Chart/Area";
+export { default as Bar } from "./Chart/Bar";
+export { default as ColorMapping } from "./Chart/ColorMapping";
+export { default as Financial } from "./Chart/Financial";
+export { default as Line } from "./Chart/Line";
+export { default as Pie } from "./Chart/Pie";
+export { default as Pyramid } from "./Chart/Pyramid";
+export { default as Stacked } from "./Chart/Stacked";
